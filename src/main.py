20fdba2887ae8fed3905a8eeebb6a17d4a402d67
272188 <sheila.psb@gmail.com>
2023-08-21 23:17:36 -0300

    comentarios do codigo

--- src/main.py
+++ src/main.py
@@ -1,5 +1,12 @@
+# Importa a classe Qr do módulo QrUtilitario.Qr
 from QrUtilitario.Qr import Qr
+
+# Define uma palavra que será transformada em QR Code
 palavra = "+55(89)994545850"
 #"Suco de cevadiss, é um leite divinis, qui tem lupuliz, matis, aguis e fermentis"
+
+# Gera um QR Code a partir da palavra usando o método estático gerar_qr da classe Qr
+# O resultado é armazenado na variável 'dado'
 dado = Qr.gerar_qr(palavra)
+# Ler uma imagem contendo um QR Code usando o método estático ler_imagem da classe Qr
 Qr.ler_imagem("meu_qr_code.png")
